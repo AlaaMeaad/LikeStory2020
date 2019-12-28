76e532a6db10862deee1b35a2399cb2b9809b916
AlaaMeaad <3la2.me3ad@Gmail.com>
2019-12-28 18:21:39 +0200

    1.1

---
+++ app/src/main/java/com/alaameaad/likestory/MainActivity.java
@@ -0,0 +1,28 @@
+package com.alaameaad.likestory;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private NavController navController;
+    private NavHostFragment navHostFragment;
+    private Toolbar mToolbar;
+    private AppBarConfiguration appBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
